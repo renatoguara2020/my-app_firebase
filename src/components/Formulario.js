4072da2c52eb51a608c74b391ecd7acc6635033b
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-16 11:45:23 -0300

    ok

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {db} from '../FirebaseConnection';
-import { collection, addDoc} from 'firebase/firestore'
+import { collection, addDoc} from 'firebase/firestore';
+import '../app.css';
 
 
 
@@ -25,27 +26,28 @@ const  Formulario = () => {
       setFirstName('');
       setLastName('');
       setEmail('');
+      setMensagem('');
     })
     .catch((error) => {
       console.log("ERRO " + error)
     })
   }
   return (
-    <div>
+    <div className="container">
       <h1>Curso de React com Firebase 2023 :) !!!</h1>
 
       <div className="container-fluid">
         
             <label className="form-label">Firstname:</label>
-            <input type="text" className="form-control" placeholder="Digite o título" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+            <input type="text" className="form-control" placeholder="Digite o seu nome" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
 
             <label className='form-label'>Lastname:</label>
-            <input className="form-control" type="text" placeholder="Digite o autor" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+            <input className="form-control" type="text" placeholder="Digite o seu sobrenome" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
 
             <label className="form-label">Email:</label>
-            <input className="form-control" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input className="form-control" type="email" placeholder="Digite seu E_mail" value={email} onChange={(e) => setEmail(e.target.value)} />
             <label className="form-label">Mensagem:</label>
-            <input className="form-control" type="text" value={mensagem} onChange={(e) => setMensagem(e.target.value)} /><br></br>
+            <input className="form-control" type="text" placeholder="Digite a sua Mensagem" value={mensagem} onChange={(e) => setMensagem(e.target.value)} /><br></br>
             <button className="btn btn-primary" onClick={handleAdd}>Cadastrar</button>
         
         </div>
