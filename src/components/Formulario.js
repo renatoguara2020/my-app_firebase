7f72bfda07bd69354493fcdc6397de5da599de86
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-16 08:21:27 -0300

    ok

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -34,13 +34,15 @@ const  Formulario = () => {
       <h1>Curso de React com Firebase 2023 :) !!!</h1>
 
       <div className="container-fluid">
-      <label className="form-label">Título:</label>
-      <input type="text" className="form-control" placeholder="Digite o título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
+        
+            <label className="form-label">Título:</label>
+            <input type="text" className="form-control" placeholder="Digite o título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
 
-      <label className='form-label'>Autor:</label>
-      <input className="form-control" type="text" placeholder="Digite o autor" value={autor} onChange={(e) => setAutor(e.target.value)}/><br></br>
-      <button className="btn btn-primary" onClick={handleAdd}>Cadastrar</button>
-      </div>
+            <label className='form-label'>Autor:</label>
+            <input className="form-control" type="text" placeholder="Digite o autor" value={autor} onChange={(e) => setAutor(e.target.value)}/><br></br>
+            <button className="btn btn-primary" onClick={handleAdd}>Cadastrar</button>
+        
+        </div>
     </div>
   );
 }
