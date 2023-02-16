9c3b36c8b21ac0a06a2f1a7b35af2105b23c73a3
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-16 09:04:50 -0300

    ok

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -19,22 +19,21 @@ const  Formulario = () => {
     await addDoc(collection(db, "posts"), {
       titulo: titulo,
       autor: autor,
-    })
-    .then(() => {
+    }).then(() => {
      
       setAutor('');
       setTitulo('');
-      alert("Posts cadastrados com sucesso!!!!");
-    })
-    .catch((error) => {
+      console.log("Posts cadastrados com sucesso!!!!");
+    }).catch((error) => {
       console.log("ERRO " + error)
     })
   }
   return (
-    <div>
+    <div className="container-fluid">
       <h1>Curso de React com Firebase 2023 :) !!!</h1>
 
-      <div className="container-fluid">
+      <div>
+      <wr/>
         
             <label className="form-label">Título:</label>
             <input type="text" className="form-control" placeholder="Digite o título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
@@ -42,9 +41,10 @@ const  Formulario = () => {
             <label className='form-label'>Autor:</label>
             <input className="form-control" type="text" placeholder="Digite o autor" value={autor} onChange={(e) => setAutor(e.target.value)}/><br></br>
             <button className="btn btn-outline-dark btn-lg" onClick={handleAdd}>Cadastrar</button>
-        
+        <wr/>
         </div>
     </div>
+    
   );
 }
 
