b2b883f6ba96d4e45b4caa303d7b3690ef12110d
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-16 07:54:16 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
-import { useState } from 'react';
-import {db} from './FirebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
-import './app.css';
+import Formulario from "./components/Formulario"
 
 
-const  App = () => {
+const App = () =>{
 
-  const [titulo, setTitulo] = useState('');
-  const [autor, setAutor] = useState('');
 
-  async function handleAdd(){
+  return(
 
-    // await setDoc(doc(db, "posts", "12345"), {
-    //   titulo: titulo,
-    //   autor: autor,
-    // }).then(() =>{console.log("Dados registrados no banco Firebase")}).catch((error) => {console.log("Gerou erro!!" + error.message)});
-    
-    await addDoc(collection(db, "posts"), {
-      titulo: titulo,
-      autor: autor,
-    })
-    .then(() => {
-      console.log("CADASTRADO COM SUCESSO")
-      setAutor('');
-      setTitulo('')
-    })
-    .catch((error) => {
-      console.log("ERRO " + error)
-    })
-  }
-  return (
     <div>
-      <h1>Curso de React com Firebase 2023 :) !!!</h1>
-
-      <div className="container">
-      <label className="form-label">Título:</label>
-      <input type="text" className="form-control" placeholder="Digite o título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
-
-      <label className='form-label'>Autor:</label>
-      <input className="form-control" type="text" placeholder="Digite o autor" value={autor} onChange={(e) => setAutor(e.target.value)}/><br></br>
-      <button className="btn btn-success" onClick={handleAdd}>Cadastrar</button>
-      </div>
+      <h1>React com Firebase</h1>
+      <Formulario />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import {db} from '../FirebaseConnection';
+import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
+
+
+
+const  Formulario = () => {
+
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd(){
+
+    // await setDoc(doc(db, "posts", "12345"), {
+    //   titulo: titulo,
+    //   autor: autor,
+    // }).then(() =>{console.log("Dados registrados no banco Firebase")}).catch((error) => {console.log("Gerou erro!!" + error.message)});
+    
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(() => {
+      console.log("CADASTRADO COM SUCESSO")
+      setAutor('');
+      setTitulo('')
+    })
+    .catch((error) => {
+      console.log("ERRO " + error)
+    })
+  }
+  return (
+    <div>
+      <h1>Curso de React com Firebase 2023 :) !!!</h1>
+
+      <div className="container">
+      <label className="form-label">Título:</label>
+      <input type="text" className="form-control" placeholder="Digite o título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
+
+      <label className='form-label'>Autor:</label>
+      <input className="form-control" type="text" placeholder="Digite o autor" value={autor} onChange={(e) => setAutor(e.target.value)}/><br></br>
+      <button className="btn btn-success" onClick={handleAdd}>Cadastrar</button>
+      </div>
+    </div>
+  );
+}
+
+export default Formulario;
