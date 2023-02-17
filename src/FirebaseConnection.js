caaa5e8afbd2208844afbfefa913454f3e71ca61
RENATO ALVES SOARES <68010466+renatoguara2020@users.noreply.github.com>
2023-02-17 12:49:18 -0300

    Update FirebaseConnection.js

--- src/FirebaseConnection.js
+++ src/FirebaseConnection.js
@@ -4,9 +4,7 @@ import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
 
     
-    projectId: "react-2023-matheus",
-    storageBucket: "react-2023-matheus.appspot.com",
-    messagingSenderId: "889383050297",
+    
     appId: "1:889383050297:web:f42cc60d133d686e0641e8",
     measurementId: "G-CY1JKB8NNM"
   };
